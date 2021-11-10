00b0779839f97bc0c5b15a0ac91eb45b185f7b57
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2021-11-10 12:01:47 -0800

    Create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = WIDTH * 9 / 16;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = vec![0u32; WIDTH * HEIGHT];
+
+    let mut window =
+        Window::new("Fractal Explorer", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
 }
